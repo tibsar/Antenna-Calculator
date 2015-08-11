329b3c7ce9b597246841698ac82c7d28f0937d52
tibsar <sxt4866@rit.edu>
2015-08-11 17:43:00 -0400

    styles page

--- js/calculate.js
+++ js/calculate.js
@@ -3,5 +3,6 @@ function calculate() {
   var wavelength = parseInt($("#wavelength").val());
   var full = 984;
   var length_feet = (full*(wavelength/8))/frequency;
-  $("#feet").text(length_feet + " feet.")
+  $("#length").text("The length is:")
+  $("#feet").text(length_feet + " feet")
 }
